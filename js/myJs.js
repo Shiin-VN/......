2001120d16bf34fab06a9f1d72996bab6e0bc71e
Shiin-VN <nguyenvanquyet.id2021@gmail.com>
2024-08-27 21:04:40 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://shiin-vn.github.io/crussh/";
+            window.location = "https://shiin-vn.github.io/Crush-demo/";
           },
         });
       }
